6d7538efbed1a0aa33890935b48820bc8fc80dcc
SimonjShurety <simonjshurety@gmail.com>
2022-11-21 08:52:25 -0300

    Add array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import * as React from "react";
 
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-function getTitle(title) {
-  return title;
-}
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>Hello {list[0].title}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
