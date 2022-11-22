9d9d24e06c056454fa264392fdcb03fa7dd66ee3
SimonjShurety <simonjshurety@gmail.com>
2022-11-21 21:02:17 -0300

    Add span JSX items

--- src/App.js
+++ src/App.js
@@ -25,6 +25,21 @@ function App() {
       <h1>Hello {list[0].title}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
