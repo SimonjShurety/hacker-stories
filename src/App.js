e3ef5afab07a257daf1f3cf7a1c0329fe1f54490
SimonjShurety <simonjshurety@gmail.com>
2022-12-28 12:07:04 -0300

    Introduce loading indicator and error handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-// P93
+// P103
 
 const initialStories = [
   {
@@ -40,10 +40,17 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
   React.useEffect(() => {
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+    getAsyncStories()
+      .then((result) => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
@@ -75,7 +82,13 @@ const App = () => {
       </InputWithLabel>
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 };
