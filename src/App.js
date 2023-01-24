addb4ba2aa50965b5f71ac45073d4f2bc28f8bc1
SimonjShurety <simonjshurety@gmail.com>
2023-01-24 12:23:55 -0300

    Memoized Handler

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ const App = () => {
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
@@ -75,6 +75,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
